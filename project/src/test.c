41ed727605ec1cf9b706db67df4d88ed157efe6c
Tatsuya Nagamori <nagamori@orientalmotor.co.jp>
2023-07-06 14:34:34 +0900

    ターミナルソフト

--- project/src/test.c
+++ project/src/test.c
@@ -1,7 +1,8 @@
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <tchar.h> 
+#include <tchar.h>    /* _T */
+#include <conio.h>    /* _kbhit  _getch */
 #include "test.h"
 
 static int comm_ComOpen(HANDLE *pHndl);
@@ -19,26 +20,35 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst,LPSTR lpszCmdLine, in
 	}
 
     printf("succeed COM open\n");
-    DWORD dwSize;
-    char wrData[16] = {0};
-    wrData[0] = 0x1b; /* ESC */
-     WriteFile(handleComm,&wrData[0],1,&dwSize,NULL);
-    Sleep(10);
-    printf("esc write\n");
+    printf("\n-----------------------------------------------\n");
 
     int flg = 0;
     while(0==flg){
-        char rdC;
-        DWORD drSize = 0;
-        BOOL res = ReadFile(handleComm,&rdC,1,&drSize,NULL);
-        if( (TRUE == res)&&(1 == drSize) ){
-            putchar(rdC);
-        }else{
-            flg = 1;
+        while(1){
+            char rdC;
+            DWORD drSize = 0;
+            BOOL res = ReadFile(handleComm,&rdC,1,&drSize,NULL);
+            if( (TRUE == res)&&(1 == drSize) ){
+                putchar(rdC);
+            }else{
+                break;
+            }
+        }
+
+        DWORD dwSize;
+        char wrC;
+        if(0 != _kbhit()){
+            wrC = _getch();
+            if('!' == wrC){
+                flg = 1;
+            }else
+            if(EOF != wrC){
+                WriteFile(handleComm,&wrC,1,&dwSize,NULL);
+            }else{}
         }
     }
-
-    printf("\nread\n");
+    CloseHandle(handleComm);
+    printf("\n-----------------------------------------------\n");
 
 	return 0;   /* 正常終了 */
 }
@@ -66,7 +76,7 @@ static int comm_ComOpen(HANDLE *pHndl)
 	timeout.ReadTotalTimeoutMultiplier	= 0;				/* 読込の１文字あたりの時間(for Win9x)			*/
 	timeout.ReadTotalTimeoutConstant	= 0;				/* 読込エラー検出用のタイムアウト時間			*/
 	timeout.WriteTotalTimeoutMultiplier = 0;				/* 書き込み１文字あたりの待ち時間(for Win9x)	*/
-	timeout.WriteTotalTimeoutConstant	= 1000;         	/* 書き込みエラー検出用のタイムアウト時間		*/
+	timeout.WriteTotalTimeoutConstant	= 100;         	    /* 書き込みエラー検出用のタイムアウト時間		*/
 
 /*	timeout.ReadIntervalTimeout			= 100;	*/	/* 文字読込時の２も時間の全体待ち時間（msec）	*/
 /*	timeout.ReadTotalTimeoutMultiplier	= 5;	*/	/* 読込の１文字あたりの時間(for Win9x)			*/
